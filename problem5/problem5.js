d4f80bf22d6cb51c2f43ca5981144a5a5ceb32ef
developer-shourav <mrinmoyraj823@gmail.com>
2023-01-29 15:30:49 +0600

    problem 5 solved

---
+++ problem5/problem5.js
@@ -0,0 +1,184 @@
+
+
+ var aliyasResult = 95;
+ var daliyasResult = 66;
+ var saliyasResult = 80;
+ var milyasResult = 59;
+ var liliyasResult = 47;
+ var jalaiyasResult = 77;
+
+
+
+ /* ---------------------------------------------
+                   Aliya's Grade
+---------------------------------------------- */
+if(  50 > aliyasResult){
+    console.log("Aliya You got " + aliyasResult + ", unfortunately you failed in this Exam !!!");
+ }
+
+ else if ( 50 <= aliyasResult && aliyasResult < 60){
+    console.log("Congratulations Aliya!!!   You got " + aliyasResult + ". And you Grade is 'D' ");
+ }
+ 
+
+ else if ( 60 <= aliyasResult && aliyasResult < 70){
+    console.log("Congratulations Aliya!!!   You got " + aliyasResult + ". And you Grade is 'C' ");
+ }
+
+ else if ( 70 <= aliyasResult && aliyasResult < 80){
+    console.log("Congratulations Aliya!!!   You got " + aliyasResult + ". And you Grade is 'B' ");
+ }
+
+ else if ( 80 <= aliyasResult && aliyasResult < 90){
+    console.log("Congratulations Aliya!!!   You got " + aliyasResult + ". And you Grade is 'A' ");
+ }
+
+ else {
+    console.log("Wow 😲😲😲  Wonderful 💖💐🎉 Aliya !!! You got " + aliyasResult + ". And you Grade is 'A+' ");
+ }
+
+
+/* ---------------------------------------------
+                   Daliya's Grade
+---------------------------------------------- */
+if(  50 > daliyasResult){
+    console.log("Daliya You got " + daliyasResult + ", unfortunately you failed in this Exam !!!");
+ }
+
+ else if ( 50 <= daliyasResult && daliyasResult < 60){
+    console.log("Congratulations Daliya!!!   You got " + daliyasResult + ". And you Grade is 'D' ");
+ }
+ 
+
+ else if ( 60 <= daliyasResult && daliyasResult < 70){
+    console.log("Congratulations Daliya!!!   You got " + daliyasResult + ". And you Grade is 'C' ");
+ }
+
+ else if ( 70 <= daliyasResult && daliyasResult < 80){
+    console.log("Congratulations Daliya!!!   You got " + daliyasResult + ". And you Grade is 'B' ");
+ }
+
+ else if ( 80 <= daliyasResult && daliyasResult < 90){
+    console.log("Congratulations Daliya!!!   You got " + daliyasResult + ". And you Grade is 'A' ");
+ }
+
+ else {
+    console.log("Wow 😲😲😲  Wonderful 💖💐🎉 Daliya !!! You got " + daliyasResult + ". And you Grade is 'A+' ");
+ }
+
+
+
+ /* ---------------------------------------------
+                   Saliya's Grade
+---------------------------------------------- */
+if(  50 > saliyasResult){
+    console.log("Saliya You got " + saliyasResult + ", unfortunately you failed in this Exam !!!");
+ }
+
+ else if ( 50 <= saliyasResult && saliyasResult < 60){
+    console.log("Congratulations Saliya!!!   You got " + saliyasResult + ". And you Grade is 'D' ");
+ }
+ 
+
+ else if ( 60 <= saliyasResult && saliyasResult < 70){
+    console.log("Congratulations Saliya!!!   You got " + saliyasResult + ". And you Grade is 'C' ");
+ }
+
+ else if ( 70 <= saliyasResult && saliyasResult < 80){
+    console.log("Congratulations Saliya!!!   You got " + saliyasResult + ". And you Grade is 'B' ");
+ }
+
+ else if ( 80 <= saliyasResult && saliyasResult < 90){
+    console.log("Congratulations Saliya!!!   You got " + saliyasResult + ". And you Grade is 'A' ");
+ }
+
+ else {
+    console.log("Wow 😲😲😲  Wonderful 💖💐🎉 Saliya !!! You got " + saliyasResult + ". And you Grade is 'A+' ");
+ }
+
+
+
+ /* ---------------------------------------------
+                   Maliya's Grade
+---------------------------------------------- */
+if(  50 > milyasResult){
+    console.log("Maliya You got " + milyasResult + ", unfortunately you failed in this Exam !!!");
+ }
+
+ else if ( 50 <= milyasResult && milyasResult < 60){
+    console.log("Congratulations Maliya!!!   You got " + milyasResult + ". And you Grade is 'D' ");
+ }
+ 
+
+ else if ( 60 <= milyasResult && milyasResult < 70){
+    console.log("Congratulations Maliya!!!   You got " + milyasResult + ". And you Grade is 'C' ");
+ }
+
+ else if ( 70 <= milyasResult && milyasResult < 80){
+    console.log("Congratulations Maliya!!!   You got " + milyasResult + ". And you Grade is 'B' ");
+ }
+
+ else if ( 80 <= milyasResult && milyasResult < 90){
+    console.log("Congratulations Maliya!!!   You got " + milyasResult + ". And you Grade is 'A' ");
+ }
+
+ else {
+    console.log("Wow 😲😲😲  Wonderful 💖💐🎉 Maliya !!! You got " + milyasResult + ". And you Grade is 'A+' ");
+ }
+
+ /* ---------------------------------------------
+                   Laliyas Grade
+---------------------------------------------- */
+ if(  50 > liliyasResult){
+    console.log("Laliya You got " + liliyasResult + ", unfortunately you failed in this Exam !!!");
+ }
+
+ else if ( 50 <= liliyasResult && liliyasResult < 60){
+    console.log("Congratulations Laliya!!!   You got " + liliyasResult + ". And you Grade is 'D' ");
+ }
+ 
+
+ else if ( 60 <= liliyasResult && liliyasResult < 70){
+    console.log("Congratulations Laliya!!!   You got " + liliyasResult + ". And you Grade is 'C' ");
+ }
+
+ else if ( 70 <= liliyasResult && liliyasResult < 80){
+    console.log("Congratulations Laliya!!!   You got " + liliyasResult + ". And you Grade is 'B' ");
+ }
+
+ else if ( 80 <= liliyasResult && liliyasResult < 90){
+    console.log("Congratulations Laliya!!!   You got " + liliyasResult + ". And you Grade is 'A' ");
+ }
+
+ else {
+    console.log("Wow 😲😲😲  Wonderful 💖💐🎉 Laliya !!! You got " + liliyasResult + ". And you Grade is 'A+' ");
+ }
+
+
+ /* ---------------------------------------------
+                   Jalaiya's Grade
+---------------------------------------------- */
+if(  50 > jalaiyasResult){
+    console.log("Jalaiya You got " + jalaiyasResult + ", unfortunately you failed in this Exam !!!");
+ }
+
+ else if ( 50 <= jalaiyasResult && jalaiyasResult < 60){
+    console.log("Congratulations Jalaiya!!!   You got " + jalaiyasResult + ". And you Grade is 'D' ");
+ }
+ 
+
+ else if ( 60 <= jalaiyasResult && jalaiyasResult < 70){
+    console.log("Congratulations Jalaiya!!!   You got " + jalaiyasResult + ". And you Grade is 'C' ");
+ }
+
+ else if ( 70 <= jalaiyasResult && jalaiyasResult < 80){
+    console.log("Congratulations Jalaiya!!!   You got " + jalaiyasResult + ". And you Grade is 'B' ");
+ }
+
+ else if ( 80 <= jalaiyasResult && jalaiyasResult < 90){
+    console.log("Congratulations Jalaiya!!!   You got " + jalaiyasResult + ". And you Grade is 'A' ");
+ }
+
+ else {
+    console.log("Wow 😲😲😲  Wonderful 💖💐🎉 Jalaiya !!! You got " + jalaiyasResult + ". And you Grade is 'A+' ");
+ }
