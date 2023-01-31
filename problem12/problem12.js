56a4fc86fa8554687fbe06af0df3db55d2061577
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-01 02:29:03 +0600

    problem 12 solved

---
+++ problem12/problem12.js
@@ -0,0 +1,28 @@
+
+
+
+/*  If you have more than 80000 Taka you will buy a macbook,
+if the ammount is more than 60000 you will buy a gamming laptop, if the amount is more than 40000 you will buy a lenovo yoga. If the amount is 20000 taka then you buy a secondhand laptop. Otherwise you will use a smart phone. Now solve the problem. */
+
+
+var myBudget = 50000;
+
+if( myBudget > 80000){
+    console.log("I will buy a mac book");
+}
+
+else if( myBudget > 60000){
+    console.log("I will buy a gaming laptop. ");
+}
+
+else if (myBudget > 40000){
+    console.log("I will buy a lenovo youg");
+}
+
+else if ( myBudget > 20000){
+    console.log("I will buy a second hand laptop");
+}
+
+else{
+    console.log("I will work with my smartphone");
+}
