f644731cb7dc798445cb0a2b16ee680c8a13a9be
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-01 03:48:41 +0600

    problem 17 completed

---
+++ problem17/problem.md
@@ -0,0 +1,3 @@
+ Run a for loop. 30 to 86. And when this loop goes to 44, it will brake. Code that thing
+
+ একটা ফর লুপ চালাও। ৩০ থেকে ৮৬ পর্যন্ত। আর এই লুপ ৪৪ এ গেলে ব্রেক করবে। সেই জিনিস কোড করে দেখাও 
---
+++ problem17/problem17.js
@@ -0,0 +1,11 @@
+/* Run a for loop. 30 to 86. And when this loop goes to 44, it will brake. Code that thing */
+
+
+for( var i = 30; i <= 86; i++){
+    
+    if( i == 44){
+        break;
+    }
+   console.log(i);
+}
+
