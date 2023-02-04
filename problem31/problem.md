9d3a7afc590f9ed77176f7b2ecdf43c0743440a1
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-04 14:37:10 +0600

    problem31 solved

---
+++ problem31/problem.md
@@ -0,0 +1,3 @@
+এমন একটা ফ্যাংশনা লিখো যেটাকে তুমি ঘন্টাকে ইনপুট প্যারামিটার হিসেবে দিবে। আর সে সেই ঘন্টাকে মিনিটে কনভার্ট করে মিনিট রিটার্ন করবে। 
+
+Write a function that takes the hour as an input parameter. And it will convert those hours to minutes and return minutes.
---
+++ problem31/problem31.js
@@ -0,0 +1,18 @@
+/* এমন একটা ফ্যাংশনা লিখো যেটাকে তুমি ঘন্টাকে ইনপুট প্যারামিটার হিসেবে দিবে। 
+আর সে সেই ঘন্টাকে মিনিটে কনভার্ট করে মিনিট রিটার্ন করবে। 
+
+Write a function that takes the hour as an input parameter. 
+And it will convert those hours to minutes and return minutes. */
+
+
+function hoursToMinutes(hours){
+
+    const minutes = hours * 60;
+    return minutes;
+};
+
+
+const hoursHaveToConvert = 24;
+const convertingMinutes = hoursToMinutes(hoursHaveToConvert);
+
+console.log(hoursHaveToConvert,"hours equals to",convertingMinutes,"minutes.");
