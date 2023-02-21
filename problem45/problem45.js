5b95f0927b821ff1aa7ce02238203477180478ad
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-21 11:12:01 +0600

    problem 45 solved

--- problem45/problem45.js
+++ problem45/problem45.js
@@ -16,3 +16,22 @@ Print the result.
 */
 
 
+const findEven = nameList => {
+    
+    const evenLengthName = [];
+    for(let i = 0; i < nameList.length; i++){
+
+        const friendName = nameList[i];
+        if(friendName.length % 2 == 0){
+            evenLengthName.push(friendName);
+        }
+    }
+   
+    return evenLengthName;
+}
+
+
+const friendsName = ["Shourav", "Nirob", "Alal", "Jala", "Ashim", "Shipta", "Pritom", "Arko", "Alu"];
+
+const evenLengthFriendsNameList = findEven(friendsName);
+console.log(evenLengthFriendsNameList);
