34256a3be22514bc80c4cb43308dad8f54927975
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-21 11:45:41 +0600

    problem 47 solved successfully

--- problem47/problem47.js
+++ problem47/problem47.js
@@ -12,3 +12,16 @@ Print the result.
 problem getting source : https://drive.google.com/file/d/1Ut8t4Ed8V-U0Axtz4nbIXJhmxvMEMUe9/view
 
  */
+
+const findTheBiggestNumber = (arr1, arr2) => {
+      const combinationOfTowArr = [...arr1, ...arr2];
+      const biggestNumberIs = Math.max(...combinationOfTowArr);
+
+      return biggestNumberIs;
+};
+
+
+const numberListOne = [10, 20, 30, 50, 512, 101375];
+const numberListTwo = [60, 70, 80, 19, 100, 1375];
+const result = findTheBiggestNumber(numberListOne, numberListTwo);
+console.log(result);
