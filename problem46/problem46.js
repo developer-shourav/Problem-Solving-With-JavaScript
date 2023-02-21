75b3156a7bb6a9753c9d750d24a9d4658fa65192
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-21 11:34:00 +0600

    problem 46 solved successfully

--- problem46/problem46.js
+++ problem46/problem46.js
@@ -5,3 +5,24 @@
 3. Return the average of the sum of the squared elements
 
 **Print the result. */
+
+
+
+const complexMathOperation = nums => {
+
+   let sumOfAllNumsSqure = 0;
+
+   for(const num of nums){
+      
+      const squareOfNum = num * num;
+      sumOfAllNumsSqure = sumOfAllNumsSqure + squareOfNum ;
+   }
+
+const averageOfAllNumsTotal = sumOfAllNumsSqure / nums.length;
+return averageOfAllNumsTotal;
+}
+
+
+const numbers = [10, 13, 45, 46];
+const result = complexMathOperation(numbers);
+console.log(result.toFixed(2));
