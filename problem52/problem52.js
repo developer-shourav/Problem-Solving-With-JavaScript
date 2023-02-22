c7fbac3fefe3ff3b2cef5e72b2bd780a597e88dd
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-22 23:39:21 +0600

    problem 52 initialization complted

---
+++ problem52/problem52.js
@@ -0,0 +1,30 @@
+
+/* 
+let data = {
+    location:[
+        {
+            latitude: '34.5, 37.8',
+            longitude:'98.77, 58.7',
+            city: 'Hyderabad',
+            country: 'India',
+        }
+    ]
+};
+
+
+Console the value of city
+
+ */
+
+
+
+let data = {
+    location:[
+        {
+            latitude: '34.5, 37.8',
+            longitude:'98.77, 58.7',
+            city: 'Hyderabad',
+            country: 'India',
+        }
+    ]
+};
---
+++ problem52/problem52.md
@@ -0,0 +1,16 @@
+## Practice Problem 52: `Access Object values`
+
+
+```
+let data = {
+    location:[
+        {
+            latitude: '34.5, 37.8',
+            longitude:'98.77, 58.7',
+            city: 'Hyderabad',
+            country: 'India',
+        }
+    ]
+};
+```
+### Console the value of `city`
