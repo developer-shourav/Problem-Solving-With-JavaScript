8e9b8a62d1bc5b581a4da42ceaea97b072a2f72f
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-23 13:44:44 +0600

    problem 54 completed in complex way

--- problem54/problem54.js
+++ problem54/problem54.js
@@ -13,3 +13,37 @@ Function
 Array
 
  */
+
+
+ const highestRunScorer = players => {
+    const allPlayersRun = [];
+    for( let i = 0; i < players.length; i++){
+
+       const player = players[i];
+       const playerRun = player[1];
+       allPlayersRun.push(playerRun);  
+    }
+
+    const topRuns = Math.max(...allPlayersRun);
+
+    for( let x = 0; x < players.length; x++){
+        if(players[x][1] == topRuns){
+
+            return players[x][0];
+        }
+    }
+ }
+
+
+const playersInfo = [
+    ["Asraful", 95],
+    ["Sakib", 19],
+    ["Musfique", 35],
+    ["Mahmudullah", 45],
+    ["Mashrafee", 13]
+];
+
+const result = highestRunScorer(playersInfo);
+
+console.log(`The highest run scorer is ${result}`);
+
