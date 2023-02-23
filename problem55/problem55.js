e0d5a634a253be3b901ae5f8727aa97d890f3415
developer-shourav <mrinmoyraj823@gmail.com>
2023-02-23 14:40:43 +0600

    problem 55 solved

--- problem55/problem55.js
+++ problem55/problem55.js
@@ -25,3 +25,15 @@ let data = {
 
 };
 
+/* ==================================================
+      Method One: Using Dot(.) Notation
+===================================================== */
+const secondarySchoolLocation1 = data.Sophia.study[1].secondary[1].location;
+
+console.log(secondarySchoolLocation1);
+/* ==================================================
+      Method Two: Using Bracket[] Notation
+===================================================== */
+
+const secondarySchoolLocation2 = data['Sophia']['study'][1]['secondary'][1]['location'];
+console.log(secondarySchoolLocation2);
