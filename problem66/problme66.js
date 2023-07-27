bebf199af31d6484ce52d87addf6e22ddabf47ed
developer-shourav <mrinmoyraj823@gmail.com>
2023-07-27 22:12:14 +0600

    66 solution one added

--- problem66/problme66.js
+++ problem66/problme66.js
@@ -3,3 +3,20 @@
 
 Example Input: [2, -5, 10, -3, 7] Example Output: 19 */
 
+/* ----------------------------------
+      Solution One using for loop:
+------------------------------------- */
+
+function calculatePositiveNumbers(arrayOfNumbers){
+   
+    let total = 0;
+    for( let i = 0; i < arrayOfNumbers.length; i++){
+        if(arrayOfNumbers[i] > 0){
+            total = total + arrayOfNumbers[i];
+        }
+    }
+
+    return total;
+};
+
+console.log(calculatePositiveNumbers([2, -5, 10, -3, 7]));
