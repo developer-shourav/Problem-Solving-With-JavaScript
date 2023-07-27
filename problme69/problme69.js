928e2a3cb4ae7f08f03a8669ffeede9e679db79a
developer-shourav <mrinmoyraj823@gmail.com>
2023-07-27 22:37:12 +0600

    problme 69 complete

--- problme69/problme69.js
+++ problme69/problme69.js
@@ -4,3 +4,31 @@ Implement a simple JavaScript calculator. The calculator should take two numbers
 
 
  */
+
+const JsCalculator = (num1, num2, operator) => {
+ 
+    if( operator === '+'){
+        return num1 + num2 ;
+    }
+    else if( operator === '-'){
+        return num1 - num2;
+    }
+    else if( operator === '*'){
+        return num1 * num2;
+    }
+    else if( operator === '/'){
+        if (num2 === 0) {
+            return "Error: Division by zero is not allowed.";
+        }
+        return num1 / num2 ;
+    }
+    else if ( operator === '%'){
+        return num1 % num2 ;
+    }
+    else{
+        return 'Error: Please inter valid number and mathematical operator.';
+    }
+};
+
+const result = JsCalculator(400, 200, '+');
+console.log(result);
