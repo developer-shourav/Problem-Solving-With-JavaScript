86adc6dc64a2c54b4722ad6f53215f64ab6c038e
developer-shourav <mrinmoyraj823@gmail.com>
2023-07-27 22:30:42 +0600

    problme 67 solved

--- problme67/problem67.js
+++ problme67/problem67.js
@@ -2,3 +2,18 @@
 
 Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5] Example Output: 3 */
 
+const findMostFrequent = arrayOfNums => {
+    const frequency = {};
+    arrayOfNums.map(element => frequency[element] = frequency[element] + 1 || 1);
+ 
+    const allNumbers = Object.keys(frequency);
+    const NumbersRepetition  = Object.values(frequency);
+    const maxFrequentValue = Math.max(...NumbersRepetition);
+    const maxFrequentsIndex = NumbersRepetition.findIndex ( x => x === maxFrequentValue);
+ 
+    const mostFrequentElement = allNumbers[maxFrequentsIndex];
+    return mostFrequentElement;
+ }
+ 
+ 
+ console.log(findMostFrequent([3, 5, 2, 5, 3, 3, 1, 4, 5]));
