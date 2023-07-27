edc46b871454e78f96936064d88e3da0f1bd31b6
developer-shourav <mrinmoyraj823@gmail.com>
2023-07-27 22:42:45 +0600

    romant to Integer complete

--- problem71/problem71.js
+++ problem71/problem71.js
@@ -4,3 +4,22 @@ Implement a function that converts a Roman numeral to an integer. The function s
 
  */
 
+function romanToIntNum(romanNum) {
+    let numInt = 0;
+    const romValues = {M:1000, D:500, C:100, L:50, X:10, V:5, I:1};
+
+    for(let i = 0; i < romanNum.length; i++){
+
+        if(romValues[romanNum[i]] < romValues[romanNum[i+1]]){
+            numInt -= romValues[romanNum[i]];
+        }
+        else{
+            numInt += romValues[romanNum[i]]
+        }
+    }
+    return numInt;
+}
+
+console.log(romanToIntNum("CXM"));
+console.log(romanToIntNum("XX"));
+console.log(romanToIntNum("VI"));
