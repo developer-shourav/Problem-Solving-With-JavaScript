45b31fd75bfb07c6c8efd628127c3b25ac2f72da
developer-shourav <mrinmoyraj823@gmail.com>
2025-06-14 13:26:48 +0600

    Day 5 problem solved

---
+++ LeetCode/30daysJavaScript/5.Apply_Transform_Over_Each_Element_in_Array/arrayTransform.ts
@@ -0,0 +1,59 @@
+/*
+
+
+
+Apply Transform Over Each Element in Array
+
+Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+Output: [2,3,4]
+Explanation:
+const newArray = map(arr, plusone); // [2,3,4]
+The function increases each value in the array by one. 
+Example 2:
+
+Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+Output: [1,3,5]
+Explanation: The function increases each value by the index it resides in.
+Example 3:
+
+Input: arr = [10,20,30], fn = function constant() { return 42; }
+Output: [42,42,42]
+Explanation: The function always returns 42.
+ 
+
+Constraints:
+
+0 <= arr.length <= 1000
+-109 <= arr[i] <= 109
+fn returns an integer.
+
+
+
+*/
+
+
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+    const arr2: number[] = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const ele: number = fn(arr[i], i);
+        arr2.push(ele);
+    }
+
+    return arr2;
+};
+
+const namArr: number[] = [1, 2, 3];
+const calFn = (n: number, i: number): number => {
+    return n + i;
+};
